4354dadbbda8f9a1bf0a654939efcedf045ace0c
dumengoscar-cyber <dumeng.oscar@student.ccm.edu>
2026-09-01 16:58:38 -0400

    Complete lab1

--- BoxOffice.java
+++ BoxOffice.java
@@ -1,4 +1,16 @@
+// name: oscar dumeng
+// date: 9/1/2026
+// purpose: calculate box office
+// 
+
+import java.util.Scanner;
 public class BoxOffice {
-    
-    
+public static void main(String[] args)
+{  
+  
+System.out.println("enter movie title");
+System.out.println("enter sold amount of adult tickets");
+System.out.println("enter amount of child tickets");
+new Scanner(System.in);
+}
 }
