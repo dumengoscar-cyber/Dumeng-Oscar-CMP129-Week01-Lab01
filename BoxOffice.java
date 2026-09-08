2b5088b917cc02d1719f2b2a7ed5ccc7a67557a3
dumengoscar-cyber <dumeng.oscar@student.ccm.edu>
2026-09-08 16:55:13 -0400

    Complete Week 1 Lab 2

--- BoxOffice.java
+++ BoxOffice.java
@@ -7,10 +7,14 @@ import java.util.Scanner;
 public class BoxOffice {
 public static void main(String[] args)
 {  
-  
+  new Scanner(System.in);
 System.out.println("enter movie title");
+int adultTickets;
+int childTickets;
 System.out.println("enter sold amount of adult tickets");
+stringmovieTickets;
 System.out.println("enter amount of child tickets");
-new Scanner(System.in);
 }
 }
+
+
